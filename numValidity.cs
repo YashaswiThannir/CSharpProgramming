5d97b191ff7d85facfd34d92fedd8e454730b43e
Yashaswi Thannir <YashaswiThannir@users.noreply.github.com>
2017-04-09 15:30:41 -0700

    Checking number is valid or not

---
+++ numValidity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NumberValidity
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Please enter a number between 1  to 10");
+			var n = Console.ReadLine();
+			int num = Convert.ToInt32(n);
+			if (num >= 1 && num <= 10)
+			{
+				Console.WriteLine("valid number");
+			}
+			else
+			{
+				Console.WriteLine("invalid number");
+			}
+		}
+	}
+}
