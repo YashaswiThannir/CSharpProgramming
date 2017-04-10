6b872990625b7f0251ac170cca3316050543b152
Yashaswi Thannir <YashaswiThannir@users.noreply.github.com>
2017-04-09 19:01:23 -0700

    Speed Limit  & License Status

---
+++ speedLimit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace speedLimit
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+
+			Console.WriteLine("Please enter the speed limit in this area");
+			var sl = Console.ReadLine();
+			int spLim = Convert.ToInt32(sl);
+			Console.WriteLine("What is the speed of the car?");
+			var speed = Console.ReadLine();
+			int vehicleSp = Convert.ToInt32(speed);
+			if (vehicleSp <= spLim)
+			{
+				Console.WriteLine("OK!");
+			}
+			else 
+			{
+				
+				int demerit = (vehicleSp - spLim)/5;
+				Console.WriteLine("Demerit points: "+demerit);
+				if (demerit >= 12)
+				{ 
+					Console.WriteLine("License Suspended");
+				}
+			
+
+			}
+
+		}
+	}
+}
