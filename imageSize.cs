e24a4dc58be5347ca873e97db76913696dd654c0
Yashaswi Thannir <YashaswiThannir@users.noreply.github.com>
2017-04-09 18:39:26 -0700

    image size potrait/landscape

---
+++ imageSize.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace imageSize
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Enter the width of the image ");
+			var w = Console.ReadLine();
+			double width = Convert.ToDouble(w);
+
+			Console.WriteLine("Enter the height of the image ");
+			var h = Console.ReadLine();
+			double height = Convert.ToDouble(h);
+		
+			if (width > height)
+			{
+				Console.WriteLine("It is a landscape");
+			}
+			else 
+			{ 
+				Console.WriteLine("It is a potrait");
+			}
+
+
+		}
+	}
+}
